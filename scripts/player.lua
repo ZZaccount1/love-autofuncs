1cfbe693402686a66221d12a6d5eab4879671e96
zz zz <nmio45137@gmail.com>
2022-12-21 03:15:48 +0200

    player template example

---
+++ scripts/player.lua
@@ -0,0 +1,32 @@
+player = {}
+
+player.x = 0
+player.y = 0
+player.speed = 1
+
+local rectangleSize = 10
+
+function player:update(dt)
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+    end
+    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+        player.y = player.y + player.speed
+    end
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        player.x = player.x - player.speed    
+    end
+end
+
+function player:draw()
+    love.graphics.rectangle("fill", player.x, player.y, rectangleSize, rectangleSize)
+end
+
+return
+{
+    update = function(...) return player.update(player, ...) end,
+    draw = function(...) return player.draw(player, ...) end,
+}
