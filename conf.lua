8741aea8c28f6410661340db024963fb56308ffa
zz zz <nmio45137@gmail.com>
2023-01-05 16:35:09 +0200

    Turn off console

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "11.4"                 -- The LÖVE version this game was made for (string)
-    t.console = true                   -- Attach a console (boolean, Windows only)
+    t.console = false                  -- Attach a console (boolean, Windows only)
 
     t.window.title = "Autofuncs"       -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
